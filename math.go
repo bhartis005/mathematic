0d26953f31ca674e8191dbb3678855ff70670e6d
bhartis005 <bsharma@magassians.co>
2020-01-20 18:21:45 +0530

    Added initial math.go file

---
+++ math.go
@@ -0,0 +1,30 @@
+package mathematic
+
+// Sum ...
+func Sum(a, b int) int {
+	return sum(a, b)
+}
+
+func sum(a, b int) int {
+	return (a + b)
+}
+
+// Subtract ...
+func Subtract(a, b int) int {
+	return (a - b)
+}
+
+// Multiply ...
+func Multiply(a, b int) int {
+	return (a * b)
+}
+
+// Division ...
+func Division(a, b int) int {
+	if b == 0 {
+		return 0
+	} else {
+		return a / b
+	}
+}
+// ....
